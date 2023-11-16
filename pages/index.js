8a0b6bb589d32ef99fc80aff230a51aa9dea6f83
Jinchenko <56926910+jindukwentua@users.noreply.github.com>
2023-11-16 21:50:15 +0100

    grey hoverrrr

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ const Index = ({ posts }) => {
       <Seo title="Home | Data Scientist || Data Analyst in Lagos, Nigeria" />
       <div className="container">
         <div className="py-10 md:py-32 md:w-2/3">
-          <Heading className="text-3xl md:text-4xl font-bold mb-1">[Welcome]
+          <Heading className="text-3xl md:text-5xl font-bold mb-2">[Welcome]
             <span role="img" aria-label="wave">
               🏄🏼‍♂️
             </span>
